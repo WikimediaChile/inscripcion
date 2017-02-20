8a43b66c53256b7110f6bab39e96cad02183f662
Dennis Tobar <dennis.tobar@gmail.com>
2017-02-19 22:49:08 -0300

    Añadiendo el id de Wikipedia

---
+++ web/js/basic.js
@@ -0,0 +1,30 @@
+/*global
+$, window */
+$(function () {
+    'use strict';
+    $('.ui.checkbox').checkbox();
+    $('.wikiuser').on('change', function () {
+        $('input.id').remove();
+        $.getJSON('https://es.wikipedia.org/w/api.php?callback=?&action=query&format=json&list=users&usprop=registration%7Cgender&ususers=' + $('.wikiuser').val())
+            .done(function (data) {
+                try {
+                    var $input = $('<input>').attr({
+                            type: 'hidden',
+                            name: 'person[wikiid]',
+                            'class': 'id'
+                        }),
+                        userinfo = data.query.users[0];
+                    if (userinfo.hasOwnProperty('missing')) {
+                        window.alert('Nombre de usuario no registrado en Wikipedia!');
+                        return;
+                    }
+                    $input.val(userinfo.userid);
+                    $('form').append($input);
+                } catch (e) {
+                    window.alert('Error al recuperar el nombre de usuario de Wikipedia!');
+                    return;
+                }
+            });
+    });
+
+});
